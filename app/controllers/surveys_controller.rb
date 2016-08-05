5177743045a13a90737163c5664681c544a970d1
David Jiang <davidjgrey@gmail>
2016-08-05 16:52:34 -0700

    can add questions to a survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -9,21 +9,34 @@ class SurveysController < ApplicationController
 
   def new
     @survey = Survey.new
+    @survey.num_questions.build
   end
 
   def create
     @survey = Survey.new(survey_params)
-    fail
     if @survey.save
-      redirect_to @survey
+      redirect_to edit_survey_path(@survey)
     else
       render :new
     end
   end
 
+  def edit
+    @survey = Survey.find(params[:id])
+  end
+
+  def update
+    @survey = Survey.find(params[:id])
+    if @survey.update(survey_params)
+      redirect_to edit_survey_path(@survey)
+    else
+      render :edit
+    end
+  end
+
 private
 def survey_params
-  params.require(:survey).permit(:title, questions_attributes: [:name, :body, :min_value, :max_value, :required])
+  params.require(:survey).permit(:title, num_questions_attributes: [:name, :body, :min_value, :max_value, :required])
 end
 
 end
